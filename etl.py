ec090372fe75fe757fe023407bbb78ad22025257
sfpayvn <sfpayvn@gmail.com>
2026-04-25 20:45:10 +0700

    comit init code

--- etl.py
+++ etl.py
@@ -4,7 +4,10 @@ import time
 BASE_URL = "https://e56c-116-109-223-76.ngrok-free.app"
 TIMEZONE_OFFSET = 7
 
+
 def run_job():
+    print("🔥 SCRIPT STARTED")
+
     url = f"{BASE_URL}/core/run-eligible-tenants"
 
     params = {
@@ -13,18 +16,21 @@ def run_job():
         "timezoneOffset": TIMEZONE_OFFSET
     }
 
+    print("👉 Calling API:", url)
+    print("👉 Params:", params)
+
     for i in range(3):
         try:
             res = requests.post(url, params=params, timeout=20)
 
-            print(f"Status: {res.status_code}")
-            print(res.text)
+            print("👉 STATUS:", res.status_code)
+            print("👉 RESPONSE:", res.text)
 
             res.raise_for_status()
             return True
 
         except Exception as e:
-            print(f"Retry {i+1}: {e}")
+            print(f"❌ Retry {i+1}: {e}")
             time.sleep(2)
 
     return False
